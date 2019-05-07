20040acc5e9e7fabb47ddb216bd5ad532fedddea
DoubleDoorGames <DoubleDoorGame@gmail.com>
2019-05-07 14:38:38 +0200

    [CHANGED] made the input mode for preprocessing a parameter of the file so I can append datasets for a larger total dataset

--- preprocess.py
+++ preprocess.py
@@ -15,10 +15,10 @@ def preprocess(args, input_folders, out_dir, hparams):
 	os.makedirs(wav_dir, exist_ok=True)
 	os.makedirs(linear_dir, exist_ok=True)
 	metadata = preprocessor.build_from_path(hparams, input_folders, mel_dir, linear_dir, wav_dir, args.n_jobs, tqdm=tqdm)
-	write_metadata(metadata, out_dir)
+	write_metadata(args,metadata, out_dir)
 
-def write_metadata(metadata, out_dir):
-	with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
+def write_metadata(args,metadata, out_dir):
+	with open(os.path.join(out_dir, 'train.txt'), args.file_mode, encoding='utf-8') as f:
 		for m in metadata:
 			f.write('|'.join([str(x) for x in m]) + '\n')
 	mel_frames = sum([int(m[4]) for m in metadata])
@@ -56,14 +56,14 @@ def norm_data(args):
 		if args.voice not in supported_voices:
 			raise ValueError('Please enter a supported voice option to use from M-AILABS dataset! \n{}'.format(
 				supported_voices))
-
 		path = os.path.join(args.base_dir, args.language, 'by_book', args.voice)
+
 		supported_readers = [e for e in os.listdir(path) if os.path.isdir(os.path.join(path,e))]
 		if args.reader not in supported_readers:
 			raise ValueError('Please enter a valid reader for your language and voice settings! \n{}'.format(
 				supported_readers))
-
 		path = os.path.join(path, args.reader)
+
 		supported_books = [e for e in os.listdir(path) if os.path.isdir(os.path.join(path,e))]
 		if merge_books:
 			return [os.path.join(path, book) for book in supported_books]
@@ -96,12 +96,14 @@ def main():
 	parser.add_argument('--merge_books', default='False')
 	parser.add_argument('--book', default='northandsouth')
 	parser.add_argument('--output', default='training_data')
+	parser.add_argument('--file_mode', default='w')
 	parser.add_argument('--n_jobs', type=int, default=cpu_count())
 	args = parser.parse_args()
 
 	modified_hp = hparams.parse(args.hparams)
 
 	assert args.merge_books in ('False', 'True')
+	assert args.file_mode in ('w', 'a')
 
 	run_preprocess(args, modified_hp)
 
